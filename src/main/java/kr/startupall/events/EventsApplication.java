273c5e23dd81e8708cd8f199ce08eedc811b671c
seulgiwendy <wheejuni@gmail.com>
2018-04-11 12:01:38 +0900

    [project]initial commit

---
+++ src/main/java/kr/startupall/events/EventsApplication.java
@@ -0,0 +1,12 @@
+package kr.startupall.events;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsApplication.class, args);
+	}
+}
