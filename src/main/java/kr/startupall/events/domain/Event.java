e3a3367affe309687fc7f1dc916e0d7f297e9bd0
seulgiwendy <wheejuni@gmail.com>
2018-04-16 09:57:22 +0900

    [datamodeling]Event 클래스 작성

---
+++ src/main/java/kr/startupall/events/domain/Event.java
@@ -0,0 +1,34 @@
+package kr.startupall.events.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Builder
+public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "EVENT_ID")
+    private long id;
+
+    private String title;
+
+    private String speaker;
+
+    private LocalDateTime dateTime;
+
+    @Column(name = "DUE_DATE")
+    private LocalDateTime applyFinishTime;
+
+    @Column(columnDefinition = "TEXT NULL")
+    private String description;
+
+}
