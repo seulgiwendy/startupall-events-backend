e0e9523262e63a0f5c3809d945fa1ade081fdfdc
seulgiwendy <wheejuni@gmail.com>
2018-04-16 11:02:35 +0900

    [datamodeling]Answer 클래스 작성

---
+++ src/main/java/kr/startupall/events/domain/questions/Answer.java
@@ -0,0 +1,31 @@
+package kr.startupall.events.domain.questions;
+
+import kr.startupall.events.domain.EventApply;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Builder
+public class Answer {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ANSWER_ID")
+    private long id;
+
+    @OneToOne
+    @JoinColumn(name = "QUESTION_ID")
+    private Question question;
+
+    @ManyToOne
+    @JoinColumn(name = "APPLY_ID")
+    private EventApply apply;
+
+
+}
